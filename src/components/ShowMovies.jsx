4c5dc5e64cbd9d22ce7416a486856bbb48ae6aec
coding4freedom <chawkins2010@hotmail.com>
2024-10-14 12:29:52 -0500

    Movie card placeholder added, with styling

--- src/components/ShowMovies.jsx
+++ src/components/ShowMovies.jsx
@@ -3,12 +3,29 @@ import './ShowMovies.css';
 
 const ShowMovies = () => {
     return (
-        <div className='show'>
-            <div className='show__title--wrapper'>
-                <h3 className="show__title">Search results:</h3>                
-            </div>
+      <div className="show">
+        <div className="show__title--wrapper">
+          <h3 className="show__title">Search results for:</h3>
         </div>
-    )
+        <section className="show__movies--wrapper">
+          <div className="show__card">
+            <div className="show__card--title">
+              <h2>Title</h2>
+              <div className="card__overlay">More Info</div>
+            </div>
+            <p className='show__para'>
+                <strong>imdbID:</strong> ttsomenums              
+            </p>
+            <p className='show__para'>
+              <strong>Type:</strong> Movie
+            </p>
+            <p className='show__para'>
+                <strong>Year:</strong> 2024
+            </p>            
+          </div>
+        </section>
+      </div>
+    );
 }
 
 export default ShowMovies;
